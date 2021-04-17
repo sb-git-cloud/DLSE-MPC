2349c079d7fb7c1677b331a0dd0ec3d346f64268
sbruegge <66483176+sbruegge@users.noreply.github.com>
2021-04-16 18:41:58 -0700

    bug fixes
    networks: integration of preprocessing layer into model, deletion of additional variables for weight, deletion of obsolete function
    
    algortihms: debugging of CVX part, new MPC class, other bug fixes

--- python/algorithms.py
+++ python/algorithms.py
@@ -3,135 +3,153 @@ import cvxpy as cp
 import tensorflow as tf
 import numpy as np
 
-def mpc(state_k, dsle_net, u0, **kwargs):
-    assert u0.ndim == 1 and state_k.ndim == 1
+class MpcProblem:
+    def __init__(self, dsle_net, state_dim, inp_dim, **kwargs):
 
-    x0 = np.concatenate(state_k, u0, axis=0)
-    # Assume u0 = [u_0^{(1)} u_0^{(2)} ... u_{horizon-1}^{(m)}]^T
+        self.state_dim = state_dim  # state dimension
+        self.inp_dim = inp_dim  # input dimension
+        self.x0_cppar = cp.Parameter(state_dim)  # optimization parameter
 
-    # Adjust constraints as optimization will be over [state_k; u0]
+        # Adjust constraints as optimization will be over [state_k; u0]
+        if 'Aeq' in kwargs:  # linear equality constraints: Aeq * u == beq, combined with state_k == x0
+            assert 'beq' in kwargs
+            # Equality constraint for state_k
+            aeq = np.concatenate((np.identity(state_dim), np.zeros((state_dim, inp_dim))), axis=1)
+            beq = self.x0_cppar
 
-    # linear equality constraints combined with state_k != x0
-    if 'Aeq' in kwargs:
-        assert 'beq' in kwargs
-        # Equality constraint for state_k
-        aeq = np.concatenate((np.identity(len(state_k)), np.zeros(len(state_k), len(u0))), axis=1)
-        beq = state_k
+            # Equality constraint for input defined by user, adapted to DSLEA with x = [state_k;u0]
+            kwargs['Aeq'] = np.concatenate((np.zeros((len(kwargs['beq']), state_dim)), kwargs['Aeq']), axis=1)
+            kwargs['Aeq'] = np.concatenate((aeq, kwargs['Aeq']), axis=0)
+            kwargs['beq'] = np.concatenate((beq, kwargs['beq']), axis=0)
+        else:
+            kwargs['Aeq'] = np.concatenate((np.identity(state_dim), np.zeros((state_dim, inp_dim))), axis=1)
+            kwargs['beq'] = self.x0_cppar
 
-        # Equality constraint for input defined by user, adapted to DSLEA with x = [state_k;u0]
-        kwargs['Aeq'] = np.concatenate((np.zeros(len(kwargs['beq']), len(state_k)), kwargs['Aeq']), axis=1)
-        kwargs['Aeq'] = np.concatenate((aeq, kwargs['Aeq']), axis=0)
-        kwargs['beq'] = np.concatenate((beq, kwargs['beq']), axis=0)
-    else:
-        kwargs['Aeq'] = np.concatenate((np.identity(len(state_k)), np.zeros(len(state_k), len(u0))), axis=1)
-        kwargs['beq'] = state_k
+        if 'A' in kwargs:  # linear inequality constraints: A u <= b
+            assert 'b' in kwargs
+            kwargs['A'] = np.concatenate((np.zeros((len(kwargs['b']), state_dim)), kwargs['A']), axis=1)
 
-    # linear inequality constraints
-    if 'A' in kwargs:
-        assert 'b' in kwargs
-        kwargs['A'] = np.concatenate((np.zeros(len(kwargs['b']), len(state_k)), kwargs['A']), axis=1)
+        if 'lb' in kwargs:  # lower bound: u >= lb
+            kwargs['lb'] = np.concatenate((np.zeros((state_dim, 1), kwargs['lb'])), axis=0)
 
-    # lower bound
-    if 'lb' in kwargs:
-        kwargs['lb'] = np.concatenate((np.zeros(len(state_k), 1), kwargs['lb']), axis=0)
+        if 'ub' in kwargs:  # upper bound: u <= ub
+            kwargs['ub'] = np.concatenate((np.zeros((state_dim, 1), kwargs['ub'])), axis=0)
 
-    # upper bound
-    if 'ub' in kwargs:
-        kwargs['ub'] = np.concatenate((np.zeros(len(state_k), 1), kwargs['ub']), axis=0)
+        self.dslea_problem = DsleaProblem(dsle_net, state_dim+inp_dim, **kwargs)
 
-    x, y = dslea(x0, dsle_net, kwargs)
-    return x[len(state_k):]
+    def solve(self, state_k, u0, verbose=False, step_tol=1e-6, MAX_ITERS = 1e3, **kwargs):
+        self.x0_cppar.value = state_k
+        x0 = np.concatenate(state_k, u0, axis=0)
+        xu, dh = self.dslea_problem.solve(x0, verbose, step_tol, MAX_ITERS, kwargs)
+        return xu[self.state_dim:]  # return only the control sequence
 
-def dslea(x0, dsle_net, **kwargs):
-    delta = sys.float_info.max
-    constr = []
-    x_cpvar = cp.Variable(x0.size)
+class DsleaProblem:
+    def __init__(self, dsle_net, n_inputs, **kwargs):
 
-    # Set stopping criterion parameter
-    if not ('delta_tol' in kwargs):
-        delta_tol = 1e-9
-    else:
-        delta_tol = kwargs['delta_tol']
+        # Extract network parameters from model for faster access during optimization
+        # Preprocessing layers
+        self.mean_in = tf.make_ndarray(tf.make_tensor_proto(dsle_net.get_layer('prepro_input').mean))  # input
+        self.var_in = tf.make_ndarray(tf.make_tensor_proto(dsle_net.get_layer('prepro_input').variance))  # input
+        self.mean_out = float(tf.make_ndarray(tf.make_tensor_proto(dsle_net.get_layer('prepro_output').mean)))  # output
+        self.var_out = float(tf.make_ndarray(tf.make_tensor_proto(dsle_net.get_layer('prepro_output').variance)))  # out
+        # Top Log-sum-exp layer
+        self.kWeightsTop = dsle_net.get_layer('top').get_weights()[0]
+        self.kBiasTop = dsle_net.get_layer('top').get_weights()[1]
+        # Bottom log-sum-exp layer
+        self.kWeightsBtm = dsle_net.get_layer('btm').get_weights()[0]
+        self.kBiasBtm = dsle_net.get_layer('btm').get_weights()[1]
 
-    # Constraints:
-    # linear inequality constraints
-    if 'A' in kwargs:
-        assert 'b' in kwargs
-        constr += kwargs['A'] * x_cpvar - kwargs['b'] <= 0
+        # Prepare optimization problem fro CVX
+        self.x_cpvar = cp.Variable(n_inputs)
+        self.dh_cppar = cp.Parameter(n_inputs)
 
-    # linear equality constraints
-    if 'Aeq' in kwargs:
-        assert 'beq' in kwargs
-        constr += kwargs['Aeq'] * x_cpvar - kwargs['beq'] == 0
+        # Constraints:
+        self.constr = []
+        if 'A' in kwargs:  # linear inequality constraints: A * x <= b
+            assert 'b' in kwargs
+            self.constr += [kwargs['A'] @ self.x_cpvar <= kwargs['b']]
 
-    # lower bound
-    if 'lb' in kwargs:
-        constr += x_cpvar - kwargs['lb'] >= 0
+        if 'Aeq' in kwargs:  # linear equality constraints:  Aeq * x == beq
+            assert 'beq' in kwargs
+            self.constr += [kwargs['Aeq'] @ self.x_cpvar == kwargs['beq']]
 
-    # upper bound
-    if 'ub' in kwargs:
-        constr += x_cpvar - kwargs['ub'] <= 0
+        if 'lb' in kwargs:  # lower bound: x >= lb
+            self.constr += [self.x_cpvar >= kwargs['lb']]
 
-    # Start optimization via cvx
-    xk = x0
-    while delta > delta_tol:
-        yk = dh(xk, dsle_net)  # step 2 if DSLEA (solution of dual problem)
-        cost = gstar(x_cpvar, yk, dsle_net)  # define cost
-        problem = cp.Problem(cp.Minimize(cost), constr)  # set up problem
-        problem.solve()  # step 3 solve convex problem
-        delta = np.linalg.norm(xk-x_cpvar.value)/(1+np.linalg.norm(xk))
+        if 'ub' in kwargs:  # upper bound: x <= ub
+            self.constr += [self.x_cpvar <= kwargs['ub']]
 
-    return xk, yk
+        # Initialize optimization problem
+        self.delta = sys.float_info.max
+        self.cost = self.gstar(self.x_cpvar, self.dh_cppar)  # define cost
+        self.cp_problem = cp.Problem(cp.Minimize(self.cost), self.constr)  # set up problem
 
-def dh(x, dsle_net):
+    def solve(self, x0, verbose=False, step_tol=1e-6, MAX_ITERS = 1e3, **kwargs):
 
-    # Computation following
-    #   G.C.Calafiore, S.Gaubert, and C.Possieri, “A universal approximation result for difference of log - sum -
-    #   exp neural networks,” IEEE Transactions on Neural Networks and Learning Systems, 2020.
-    #   DOI: 10.1109 / TNNLS.2020.2975051.
-    #
-    # and
-    #
-    #   S. Br{\"u}ggemann and C. Possieri, "On the Use of Difference of Log-Sum-Exp Neural Networks to Solve
-    #   Data-Driven Model Predictive Control Tracking Problems," in IEEE Control Systems Letters, vol. 5, no. 4,
-    #   pp. 1267-1272, Oct. 2021, doi: 10.1109/LCSYS.2020.3032083.
-    #
-    # This function corresponds to Step 2 of Algorithm 1(DLSEA) in the later publication.
-    #
-    #
-    # The following is the sum of the numerator with each term corresponding to one row in the vector, excluding the
-    # vector multiplication, i.e.,
-    #
-    #   vec_exp[k] = exp(\gamma ^ {k} \chi) +\delta_k,
-    #
-    # where \gamma ^ {k} is the weight row vector for neuron k of the bottom layer (w_btm),  \chi the current
-    # solution of the primal problem(x) and delta_k the bias of neuron k of the bottom layer(bias_btm).
-    #
-    # We additionally take into account the normalization of the input here via var_in_inverse and it_mean_in.
+        # Setup optimization problem
+        step = sys.float_info.max
+        xk = x0
+        i = 0
+        while step > step_tol and i < MAX_ITERS:
+            self.dh_cppar.value = self.dh(xk)  # step 2 if DSLEA (solution of dual problem)
+            self.cp_problem.solve(verbose=verbose)  # step 3 solve convex problem
+            step = np.linalg.norm(xk-self.x_cpvar.value)/(1+np.linalg.norm(xk))
+            i += 1
 
-    var_in_inverse = np.diag(np.reciprocal(dsle_net.var_in))
-    vec_exp = np.exp(np.matmul(np.matmul(np.transpose(dsle_net.kWeightsBtm), var_in_inverse), (x - dsle_net.mean_in))
-                     + np.transpose(dsle_net.kBiasBtm))
+        return xk, self.dh_cppar.value
 
-    # The return value uses this vector to
-    #   a) compute the numerator by the matrix multiplication
-    #
-    #       [w_btm[:, 1] w_btm[:, 2] ... w_btm[:, K]] * [vec_exp[1] vec_exp[2] ... vec_exp[K]]^T
-    #
-    #       and an additional scaling via diag(var_in).
-    #
-    #   b) compute the denominator as the sum over vec_exp and the scaling factor of the output.The offset of the
-    #      output is absent as this is the derivative of h.
+    def dh(self, x):
 
-    return np.matmul(np.matmul(var_in_inverse, dsle_net.kWeightsBtm), vec_exp) / (np.sum(vec_exp) * dsle_net.var_out)
+        # Computation following
+        #   G.C.Calafiore, S.Gaubert, and C.Possieri, “A universal approximation result for difference of log - sum -
+        #   exp neural networks,” IEEE Transactions on Neural Networks and Learning Systems, 2020.
+        #   DOI: 10.1109 / TNNLS.2020.2975051.
+        #
+        # and
+        #
+        #   S. Br{\"u}ggemann and C. Possieri, "On the Use of Difference of Log-Sum-Exp Neural Networks to Solve
+        #   Data-Driven Model Predictive Control Tracking Problems," in IEEE Control Systems Letters, vol. 5, no. 4,
+        #   pp. 1267-1272, Oct. 2021, doi: 10.1109/LCSYS.2020.3032083.
+        #
+        # This function corresponds to Step 2 of Algorithm 1(DLSEA) in the later publication.
+        #
+        #
+        # The following is the sum of the numerator with each term corresponding to one row in the vector, excluding the
+        # vector multiplication, i.e.,
+        #
+        #   vec_exp[k] = exp(\gamma ^ {k} \chi) +\delta_k,
+        #
+        # where \gamma ^ {k} is the weight row vector for neuron k of the bottom layer (w_btm),  \chi the current
+        # solution of the primal problem(x) and delta_k the bias of neuron k of the bottom layer(bias_btm).
+        #
+        # We additionally take into account the normalization of the input here via var_in_inverse and it_mean_in.
 
-def gstar(x, dh, dsle_net):
+        var_in_inverse = np.diag(np.reciprocal(self.var_in))
+        vec_exp = np.exp(np.matmul(np.matmul(np.transpose(self.kWeightsBtm), var_in_inverse), (x - self.mean_in))
+                         + np.transpose(self.kBiasBtm))
 
-    # This corresponds to Step 3 of Algprithm 1 DSLEA and corresponds to the primal problem with linearized
-    # concave part. See detailed comments for computation in function dh.
+        # The return value uses this vector to
+        #   a) compute the numerator by the matrix multiplication
+        #
+        #       [w_btm[:, 1] w_btm[:, 2] ... w_btm[:, K]] * [vec_exp[1] vec_exp[2] ... vec_exp[K]]^T
+        #
+        #       and an additional scaling via diag(var_in).
+        #
+        #   b) compute the denominator as the sum over vec_exp and the scaling factor of the output.The offset of the
+        #      output is absent as this is the derivative of h.
 
-    var_in_inverse = np.diag(np.reciprocal(dsle_net.var_in))
-    vec_exp = np.exp(np.matmul(np.matmul(np.transpose(dsle_net.kWeightsTop), var_in_inverse),  (x - dsle_net.mean_in))
-                     + np.transpose(dsle_net.kBiasTop))
+        return np.matmul(np.matmul(var_in_inverse, self.kWeightsBtm), vec_exp) / (np.sum(vec_exp) * self.var_out)
 
-    return np.log(np.sum(vec_exp))-dsle_net.mean_out/dsle_net.var_out-x.dot(dh)
+    def gstar(self, x, dh):
+
+        # This corresponds to Step 3 of Algprithm 1 DSLEA and corresponds to the primal problem with linearized
+        # concave part. See detailed comments for computation in function dh.
+        #
+        # Instead of numpy, we use expressions from cvxpy, which are equivalent
+
+        var_in_inverse = cp.diag(cp.inv_pos(self.var_in))
+        # vec_exp = cp.exp(cp.matmul(cp.matmul(cp.transpose(self.kWeightsTop), var_in_inverse), (x-self.mean_in))
+        #                  + cp.transpose(self.kBiasTop))
+        # return cp.log(cp.sum(vec_exp)) - self.mean_out / self.var_out - cp.transpose(x)@dh
+        return cp.log_sum_exp(cp.matmul(cp.matmul(cp.transpose(self.kWeightsTop), var_in_inverse), (x-self.mean_in))
+                              + cp.transpose(self.kBiasTop)) - self.mean_out / self.var_out - cp.transpose(x)@dh
--- python/networks.py
+++ python/networks.py
@@ -1,12 +1,9 @@
 # Import packages
-import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Input, Subtract, Activation, Layer
 from tensorflow.keras import backend
 from tensorflow.keras.initializers import Ones
 import numpy as np
-import pickle
-from dsle_mpc import data
 
 class Exploglayer(Layer):
 
@@ -27,82 +24,45 @@ class Exploglayer(Layer):
 
 class Dslenet(Model):
 
-    def __init__(self, nhiddenLayers=10):
+    def __init__(self, data, nhiddenLayers=10):
         super(Dslenet, self).__init__()
+        self.layernorminput = data.layernorminput
+        self.layernorminput._name = 'prepro_input'
+
+        self.layernormoutput = data.layernormoutput
+        self.layernormoutput._name = 'prepro_output'
+
         self.top = Exploglayer(nhiddenLayers)
         self.top._name = 'top'
         self.bottom = Exploglayer(nhiddenLayers)
         self.bottom._name = 'btm'
 
     def call(self, inputs):
+        # Preprocessing layer
+        inputs = self.layernorminput(inputs)
         # Top layers
         xtop = self.top(inputs)
         # Bottom layers
         xbottom = self.bottom(inputs)
-
-        return Subtract()([xtop, xbottom])
+        # Postprocessing layer
+        output = Subtract()([xtop, xbottom])
+        return self.layernormoutput(output)
 
     def fit(self, data, *args, **kwargs):
-        # Save nromalization parameters
-        self.layernorminput = data.layernorminput
-        self.layernormoutput = data.layernormoutput
-
-        x = data.input_norm
-        y = data.output_norm
-
-        # Reserve 67% of data for validation
-        if not ('validation_split' in kwargs) and not ('validation_data' in kwargs):
-            kwargs['validation_split'] = 0.67
-
-        # Set default to 100 epochs
-        if not ('epochs' in kwargs):
-            kwargs['epochs'] = 100
+        # Define in- and outputs
+        x = data.input
+        y = data.output
         super(Dslenet, self).fit(x, y, *args, **kwargs)
-        self.set_weights(self.get_weights())
-
-    def compile(self, *args, **kwargs):
-        # Choose mean squared error as default loss function
-        if not ('loss' in kwargs):
-            kwargs['loss'] = 'mse'
-        super(Dslenet, self).compile(*args, **kwargs)
-
-    def set_weights(self, weights):
-        # Save weights
-        self.mean_in = tf.make_ndarray(tf.make_tensor_proto(self.layernorminput.mean))
-        self.var_in = tf.make_ndarray(tf.make_tensor_proto(self.layernorminput.variance))
-        self.mean_out = float(tf.make_ndarray(tf.make_tensor_proto(self.layernormoutput.mean)))
-        self.var_out = float(tf.make_ndarray(tf.make_tensor_proto(self.layernormoutput.variance)))
-
-        self.kWeightsTop = weights[0]
-        self.kWeightsBtm = weights[3]
-        self.kBiasTop = weights[1]
-        self.kBiasBtm = weights[4]
-
-        super(Dslenet, self).set_weights(weights)
 
     def export_weights_npz(self, filename):
+        # Export weights and parameters of preprocessing layer to npz file
+        # Can be imported by C++
         np.savez(filename,
-                 kWeightsTop=self.kWeightsTop,
-                 kWeightsBtm=self.kWeightsBtm,
-                 kBiasTop=self.kBiasTop,
-                 kBiasBtm=self.kBiasBtm,
-                 kMeanIn=self.mean_in,
-                 kVarIn=self.var_in,
-                 kMeanOut=self.mean_out,
-                 kVarOut=self.var_out)
-
-    def import_weights_npz(self, filename):
-
-        data = np.load(filename)
-        self.layernorminput.mean = data['kMeanIn']
-        self.layernorminput.variance = data['kVarIn']
-        self.layernormoutput.mean = data['kMeanOut']
-        self.layernormoutput.variance = data['kVarOut']
-
-        weights = self.get_weights()
-        weights[0] = data['kWeightsTop']
-        weights[3] = data['kWeightsBtm']
-        weights[1] = data['kBiasTop']
-        weights[4] = data['kBiasBtm']
-
-        self.set_weights(weights)
+                 kWeightsTop=self.get_layer('top').get_weights()[0],
+                 kWeightsBtm=self.get_layer('btm').get_weights()[0],
+                 kBiasTop=self.get_layer('top').get_weights()[1],
+                 kBiasBtm=self.get_layer('btm').get_weights()[1],
+                 kMeanIn=self.get_layer('prepro_input').mean,
+                 kVarIn=self.get_layer('prepro_input').variance,
+                 kMeanOut=self.get_layer('prepro_output').mean,
+                 kVarOut=self.get_layer('prepro_output').variance)
